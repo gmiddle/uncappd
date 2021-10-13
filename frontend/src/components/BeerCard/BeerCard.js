b8870806ab2090b498256bada1f5bf50dbdf91ef
Garrett Middleton <garrettmiddleton@gmail.com>
2021-10-13 15:10:50 -0700

    working on a 404 resource cannot be found errors on the /beers route

--- frontend/src/components/BeerCard/BeerCard.js
+++ frontend/src/components/BeerCard/BeerCard.js
@@ -8,29 +8,29 @@ const BeerCard = ({ beer }) => {
     const { setBeerSelected } = useBeerSelected();
 
     useEffect(() => {
-        beerImgRef.current.style.backgroundImage
+        beerImgRef.current.style.backgroundImage = `url(${beer.beerImg})`
     })
 
-    // const showForm = () => {
-    //     setBeerSelected(true);
-    //     return    
-    // }
+    const showForm = () => {
+        setBeerSelected(true);
+        return    
+    }
 
     return (
         <div className="beerCard-container">
-            <div ref={beerImgRef} classNmae="beerCard-img"></div>
+            <div ref={beerImgRef} className="beerCard-img">BEER IMAGE GOES HERE</div>
             <div className="beerCard-divider"></div>
             <div className="beerCard-content"></div>
                 <h1 className="beerCard-title">{beer.name}</h1>
                 <p className="beerCard-description">{beer.description}</p>
                 <div className="beerCard-info">
-                    <p className="beerCard-info beerCard-rating-avg">Rating: ${avgRating ? `${avgRating}/5` : 'N/A'}</p>
+                    {/* <p className="beerCard-info beerCard-rating-avg">Rating: ${avgRating ? `${avgRating}/5` : 'N/A'}</p> */}
                     <p className="beerCard-info beerCard-rating-total">Total Ratings: TBC</p>
                     <p className="beerCard-info beerCard-abv">ABV %: TBC</p>
                 </div>
         </div>
         // <img>
-        
+        // TODO - img tag in beerCard component
         // </img>
     )
 
